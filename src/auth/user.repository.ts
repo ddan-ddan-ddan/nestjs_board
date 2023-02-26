fae206864fccd9664082cbb75378119679f62c30
ddan-ddan-ddan <kimm3161@naver.com>
2023-02-26 14:45:12 +0900

    brcypt, userdb

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,39 @@
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { CustomRepository } from "src/boards/typeorm-ex.decorator";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/atu.credentials.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from "bcryptjs"
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
+	constructor(@InjectRepository(User) private dataSource: DataSource) {
+        super(User, dataSource.manager)
+    }
 
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({
+            username,
+            password: hashedPassword
+        })
+
+        try {
+            await this.save(user);
+        } 
+        catch(err) {
+            if(err.code === '23505') {
+                throw new ConflictException('Existing username');
+            }
+            else {
+                throw new InternalServerErrorException();
+            }
+        }
+
+        await this.save(user);
+    }
 }
